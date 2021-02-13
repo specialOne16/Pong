60d6694a4fa351f23bf02a9f8bec9f9aac8bdcd7
specialOne16 <jundi2303@gmail.com>
2021-02-13 09:36:29 +0700

    initial project Survival Shooter from tutorial

---
+++ Survival Shooter/Assets/Scripts/Command/ShootCommand.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootCommand : Command
+{
+    PlayerShooting playerShooting;
+
+    public ShootCommand(PlayerShooting playerShooting)
+    {
+        this.playerShooting = playerShooting;
+    }
+
+    public override void Execute()
+    {
+        playerShooting.Shoot();
+    }
+
+    public override void UnExecute()
+    {
+
+    }
+}
---
+++ Survival Shooter/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 6f;
+
+    int floorMask;
+    Vector3 movement;
+    Animator anim;
+    Rigidbody playerRigidBody;
+    PlayerHealth health;
+    float camRayLength = 100f;
+
+    private void Awake()
+    {
+        floorMask = LayerMask.GetMask("Floor");
+        anim = GetComponent<Animator>();
+        playerRigidBody = GetComponent<Rigidbody>();
+        health = GetComponent<PlayerHealth>();
+    }
+
+    private void FixedUpdate()
+    { 
+        Turning();
+    }
+
+    public void Move(float h, float v)
+    {
+        if (health.currentHealth < 0) return;
+        movement.Set(h, 0, v);
+        movement = movement.normalized * speed * Time.deltaTime;
+        playerRigidBody.MovePosition(transform.position + movement);
+    }
+
+    void Turning()
+    {
+        Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit floorHit;
+
+        if(Physics.Raycast(camRay, out floorHit, camRayLength, floorMask))
+        {
+            Vector3 playerToMouse = floorHit.point - transform.position;
+            playerToMouse.y = 0;
+            Quaternion newRotation = Quaternion.LookRotation(playerToMouse);
+            playerRigidBody.MoveRotation(newRotation);
+        }
+    }
+
+    public void Animating(float h, float v)
+    {
+        bool isWalking = h != 0f || v != 0f;
+        anim.SetBool("IsWalking", isWalking);
+    }
+}
